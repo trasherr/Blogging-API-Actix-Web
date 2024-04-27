75769e3d2c1fc8e26ce490d5abb392ef3edd5e0a
trasherr <2000prakharsingh@gmail.com>
2024-04-27 16:06:03 +0530

    Error Handling (Part 4)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{error::Error, fmt::Display};
+
 use actix_web::{get, middleware::Logger, web, App, HttpServer, Responder};
 use migration::{Migrator, MigratorTrait};
 use sea_orm::{Database, DatabaseConnection};
@@ -6,10 +8,35 @@ use utils::app_state::AppState;
 mod utils;
 mod routes;
 
+#[derive(Debug)]
+struct MainError{
+    message: String
+}
 
+impl Display for MainError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f,"Error: {} ", self.message)
+    }
+}
+
+
+impl Error for MainError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+
+    fn description(&self) -> &str {
+        &self.message
+    }
+
+    fn cause(&self) -> Option<&dyn Error> {
+        self.source()
+    }
+
+}
 
 #[actix_web::main] // or #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<(),MainError> {
 
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var("RUST_LOG", "actix_web=info");
@@ -23,8 +50,15 @@ async fn main() -> std::io::Result<()> {
     let address = (*utils::constants::ADDRESS).clone();
     let database_url = (*utils::constants::DATABASE_URL).clone();
 
-    let db: DatabaseConnection = Database::connect(database_url).await.unwrap();
-    Migrator::up(&db, None).await.unwrap();
+    println!("port: {}",port);
+
+    let db: DatabaseConnection = Database::connect(database_url)
+    .await
+    .map_err(|err| MainError { message: err.to_string() })?;
+
+    Migrator::up(&db, None)
+    .await
+    .map_err(|err| MainError { message: err.to_string() })?;
 
     HttpServer::new(move || {
         App::new()
@@ -34,7 +68,9 @@ async fn main() -> std::io::Result<()> {
         .configure(routes::auth_routes::config)
         .configure(routes::user_routes::config)
     })
-    .bind((address, port))?
+    .bind((address, port))
+    .map_err(|err| MainError { message: err.to_string() })?
     .run()
     .await
+    .map_err(|err| MainError { message: err.to_string() })
 }
